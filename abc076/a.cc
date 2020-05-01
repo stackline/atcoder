803e3d8895fee673f13d5fecba420de70e199972
stackline <stackline@users.noreply.github.com>
2020-05-01 18:29:39 +0900

    ABC076 A

---
+++ abc076/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int r;
+  int g;
+  cin >> r >> g;
+  // g = (r + a) / 2
+  // g * 2 = r + a
+  // a = (g * 2) - r
+  cout << (g * 2) - r << endl;
+}
