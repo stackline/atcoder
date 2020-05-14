acf60fb82cf85804dbbce98ea178546a7baa75a6
stackline <stackline@users.noreply.github.com>
2020-05-14 15:24:53 +0900

    Add utility function to template

--- tmpl.cc
+++ tmpl.cc
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int ctoi(char c) {
+  if (c >= '0' && c <= '9') {
+    return c - '0';
+  }
+  return 0;
+}
+
 int main() {
   int a, b;
   cin >> a >> b;
