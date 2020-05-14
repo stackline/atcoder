4e60f141297e65cf38379a3510b34019d474b234
stackline <stackline@users.noreply.github.com>
2020-05-14 19:55:33 +0900

    Add utility function to template

--- tmpl.cc
+++ tmpl.cc
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+char itoc(int i) {
+  if (i >= 0 && i <= 9) {
+    return '0' + i;
+  }
+  throw invalid_argument("itoc argument is not between 0 and 9.");
+}
+
 int ctoi(char c) {
   if (c >= '0' && c <= '9') {
     return c - '0';
