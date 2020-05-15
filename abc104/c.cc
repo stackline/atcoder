27d9dcca6054d7cf72d7fa7acc0fe4ad074df47a
stackline <stackline@users.noreply.github.com>
2020-05-15 18:16:41 +0900

    ABC104 C accepted (using explanation)

--- abc104/c.cc
+++ abc104/c.cc
@@ -1,61 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int calc(int &g, int min_count, vector<int> p, vector<int> c, int sum, int count, int i) {
-  // 問題を解いた時の配点
-  sum += (i + 1) * 100;
-  p[i]--;
-  count++;
-  // コンプリートボーナス
-  if (p[i] == 0) {
-    sum += c[i];
-  }
+int d, g;
+vector<int> p;
+vector<int> c;
 
+int rec(int i, int sum, int count, string bits) {
   // ベースケース
-  // 目標点数を超えたら探索終了
-  // 別ノードを探索する際、現在のミニマムまでしか探索しない
-  if (sum >= g || count == min_count) {
+  if (sum >= g) {
     return count;
   }
-
-  // 再帰ステップ
-  // O(n) / 線形時間
-  // 1 <= pi <= 100
-  for (int j = 0; j < (int) p.size(); j++) {
-    if (p[j] > 0) {
-      int calculated = calc(g, min_count, p, c, sum, count, j);
-      min_count = min(min_count, calculated);
+  if (i == d) {
+    int pos = bits.find_last_of('0');
+    int score = (pos + 1) * 100;
+    for (int j = 0; j < (int) p.at(pos); j++) {
+      sum += score;
+      count++;
+      if (sum >= g) {
+        break;
+      }
+    }
+    if (sum >= g) {
+      return count;
+    } else {
+      return 0;
     }
   }
 
-  return min_count;
+  // 完全に解く場合
+  int p1 = rec(i + 1,
+               sum + ((i + 1) * 100 * p.at(i)) + c.at(i),
+               count + p.at(i),
+               bits + '1');
+  // 全く解かない場合
+  int p2 = rec(i + 1, sum, count, bits + '0');
+
+  if (p1 == 0) {
+    return p2;
+  }
+  if (p2 == 0) {
+    return p1;
+  }
+  return min(p1, p2);
 }
 
 int main() {
-  int d, g;
   cin >> d >> g;
-  vector<int> p(d);
-  vector<int> c(d);
-  // O(n) / 線形時間
-  // 1 <= d <= 10
+  p.resize(d);
+  c.resize(d);
   for (int i = 0; i < d; i++) {
-    cin >> p[i] >> c[i];
+    cin >> p.at(i) >> c.at(i);
   }
 
-  int min_count = 0;
-  // O(n) / 線形時間
-  // 1 <= d <= 10
-  for (int i = 0; i < d; i++) {
-    min_count += p[i];
-  }
+  string bits = "";
+  int ans = rec(0, 0, 0, bits);
 
-  // O(n log n log n-1...) / 線形時間
-  // 1 <= d <= 10
-  for (int i = 0; i < d; i++) {
-    int calculated = calc(g, min_count, p, c, 0, 0, i);
-    min_count = min(min_count, calculated);
-  }
-
-  cout << min_count << endl;
+  cout << ans << endl;
   return 0;
 }
