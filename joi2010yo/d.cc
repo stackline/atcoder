2437bcf7d95cef5a558a70c15f30b14547e6983a
stackline <stackline@users.noreply.github.com>
2020-05-31 15:44:39 +0900

    JOI2010YO D refactoring

--- joi2010yo/d.cc
+++ joi2010yo/d.cc
@@ -6,31 +6,21 @@ int main() {
   int k; // 2 <= k <= 4
   cin >> n >> k;
 
-  // 表現できる最大の数 = 99 * 4枚 = 99999999
-  vector<bool> checker(100000000, false);
-
-  vector<int> v(n);
+  vector<string> v(n);
   for (int i = 0; i < n; i++) {
     cin >> v.at(i);
   }
 
   sort(v.begin(), v.end());
-  int ans = 0;
+  set<string> s;
   do {
     string a;
     for (int i = 0; i < k; i++) {
-      a += to_string(v.at(i));
+      a += v.at(i);
     }
-    int b = stoi(a);
-
-    // パターンから生成した数字の種類をカウントアップする
-    // 2回目以降、同じ数字が発生した場合はスキップする
-    if (checker.at(b) == false) {
-      ans++;
-      checker.at(b) = true;
-    }
+    s.insert(a);
   } while (next_permutation(v.begin(), v.end()));
 
-  cout << ans << endl;
+  cout << s.size() << endl;
   return 0;
 }
