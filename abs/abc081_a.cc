9104328760cda997c7fada07bdba7284f3fa8e99
stackline <stackline@users.noreply.github.com>
2020-06-04 09:41:06 +0900

    ABS ABC081 A

---
+++ abs/abc081_a.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  int ans = 0;
+  for (int i = 0; i < (int) s.size(); i++) {
+    if (s.at(i) == '1') {
+      ans++;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
