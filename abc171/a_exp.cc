825c76168700449f0a546937f37b8e8615570513
stackline <stackline@users.noreply.github.com>
2020-06-22 09:31:20 +0900

    ABC171 A explanation

---
+++ abc171/a_exp.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  char alpha;
+  cin >> alpha;
+  cout << (isupper(alpha) ? "A" : "a") << endl;
+  return 0;
+}
