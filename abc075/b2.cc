e78c927b0b6745431af15c071ed65f99a8be23da
stackline <stackline@users.noreply.github.com>
2020-07-01 17:55:54 +0900

    ABC075 B second (with refactor)

--- abc075/b2.cc
+++ abc075/b2.cc
@@ -5,13 +5,13 @@ using ll = long long;
 int h, w;
 
 int bomb_count(vector<vector<char>> &v, int x, int y) {
-  vector<int> dx{ x - 1, x,     x + 1, x + 1, x + 1, x    , x - 1, x - 1 };
-  vector<int> dy{ y - 1, y - 1, y - 1, y,     y + 1, y + 1, y + 1, y };
+  vector<int> dx{ -1, -1, -1,  0, 0,  1, 1, 1 };
+  vector<int> dy{ -1,  0,  1, -1, 1, -1, 0, 1 };
 
   int num = 0;
   for (int i = 0; i < 8; i++) {
-    int xx = dx.at(i);
-    int yy = dy.at(i);
+    int xx = x - dx.at(i);
+    int yy = y - dy.at(i);
     if (xx >= 0 && xx < h && yy >= 0 && yy < w) {
       if (v.at(xx).at(yy) == '#') {
         num++;
