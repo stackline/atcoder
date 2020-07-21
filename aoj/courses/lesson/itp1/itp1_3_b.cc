94733b8048fbf2783ddf144360db06c609e11711
stackline <stackline@users.noreply.github.com>
2020-07-21 17:49:07 +0900

    AOJ ITP1 ITP1_3_B

---
+++ aoj/courses/lesson/itp1/itp1_3_b.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  int x;
+  int i = 1;
+  while (1) {
+    cin >> x;
+    if (x == 0) { break; }
+    cout << "Case " << i << ": " << x << endl;
+    i++;
+  }
+  return 0;
+}
