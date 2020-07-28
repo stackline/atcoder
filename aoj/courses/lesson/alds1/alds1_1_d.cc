233309a6f8795936da1cafd0eecb93d8ce29e54d
stackline <stackline@users.noreply.github.com>
2020-07-28 15:00:04 +0900

    AOJ ALDS1 ALDS1_1_D

--- aoj/courses/lesson/alds1/alds1_1_d.cc
+++ aoj/courses/lesson/alds1/alds1_1_d.cc
@@ -11,18 +11,21 @@ int main() {
     cin >> R.at(i);
   }
 
-  // O(n**2) > 10**10
-  //
-  // 5C2 = (5 * 4) / (2 * 1) = 10
-  // 200000C2 = (200_000 * 199_999) / (2 * 1) < 10**11
-  ll ans = -1000000000;
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = i + 1; j < n; j++) {
-      ll dif = R.at(j) - R.at(i);
-      ans = max(ans, dif);
-    }
+  int maxv = -1000000000;
+  int minv = R.at(0);
+  // O(N)
+  for (int i = 1; i < n; i++) {
+    // 例: 5, 3, 1, 3, 4, 3
+    //
+    // i = 1 の時、R.at(1) - R.at(0) = 3 - 5 = -2
+    // i = 2 の時、R.at(2) - R.at(1) = 1 - 3 = -2
+    // i = 3 の時、R.at(3) - R.at(2) = 3 - 1 = 2
+    // i = 4 の時、R.at(4) - R.at(2) = 4 - 1 = 3
+    // i = 5 の時、R.at(5) - R.at(2) = 3 - 1 = 2
+    maxv = max(maxv, R.at(i) - minv);
+    minv = min(minv, R.at(i)); // 次のループで利用する、i-1 までの要素の中で最小の値を取得する
   }
 
-  cout << ans << endl;
+  cout << maxv << endl;
   return 0;
 }
