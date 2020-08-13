fe736d5293fb9db0e77dcdcb0d2f92aa1feb1343
stackline <stackline@users.noreply.github.com>
2020-08-13 20:34:39 +0900

    ABC122 B

---
+++ abc/abc122/b.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+bool is_acgt(char c) {
+  if (c == 'A' || c == 'C' || c == 'G' || c == 'T') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+
+  int ans = 0;
+  int cnt = 0;
+  for (int i = 0; i < (int)s.size(); i++) {
+    char c = s.at(i);
+
+    if (is_acgt(c)) { cnt++; }
+    if (!is_acgt(c) || i == (int)s.size()-1) {
+      ans = max(ans, cnt);
+      cnt = 0;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
