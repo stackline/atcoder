dc88c942254101c6dec4f29df19cb603b08f2482
stackline <stackline@users.noreply.github.com>
2020-08-15 15:13:45 +0900

    ABC106 B refactoring (Remove unnecessary loop)

--- abc/abc106/b.cc
+++ abc/abc106/b.cc
@@ -6,21 +6,17 @@ int main() {
   int n;
   cin >> n;
 
-  vector<int> v(201, 0);
+  int ans = 0;
   for (int i = 1; i <= n; i = i+2) {
+    int cnt = 0;
     for (int j = 1; j <= i; j++) {
       // 奇数 i について 1〜i までの数字で剰余を求める
       // 割り切れたら約数なのでインクリメント
-      if (i % j == 0) {
-        v.at(i)++;
-      }
+      if (i % j == 0) { cnt++; }
     }
+    if (cnt == 8) { ans++; }
   }
 
-  int ans = 0;
-  for (int i = 1; i <= n; i++) {
-    if (v.at(i) == 8) { ans++; }
-  }
   cout << ans << endl;
   return 0;
 }
