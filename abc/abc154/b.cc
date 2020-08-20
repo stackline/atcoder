49d566b73ed873b2303dd8d60775f974958258be
stackline <stackline@users.noreply.github.com>
2020-08-20 13:47:04 +0900

    ABC154 B

---
+++ abc/abc154/b.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  string s;
+  cin >> s;
+
+  for (int i = 0; i < (int) s.size(); i++) {
+    cout << 'x';
+  }
+  cout << endl;
+  return 0;
+}
