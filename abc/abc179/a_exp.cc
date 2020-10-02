e94a93093fba70d06b3f9ce9613a3358a6e7b47d
stackline <stackline@users.noreply.github.com>
2020-10-02 11:32:36 +0900

    ABC179 A add notes

--- abc/abc179/a_exp.cc
+++ abc/abc179/a_exp.cc
@@ -12,6 +12,18 @@ int main() {
 
   int len = s.length();
 
+  // ### char 型と文字列リテラルを比較するとエラー
+  //
+  //   s.at(len-1) == "s"
+  //
+  // ### エラー
+  //
+  //   ISO C++ forbids comparison between pointer and integer [-fpermissive]
+  //
+  // "s" は文字列リテラルで char 型の配列
+  // s.at(len-1) は char 型
+  // 比較の際 char 型の配列を pointer として取り扱い、char 型を int として取り扱い、型不一致でエラー
+  //
   if (s.at(len-1) == 's') {
     s += "es";
   } else {
