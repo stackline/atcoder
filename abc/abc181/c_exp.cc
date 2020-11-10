ad3ccd3d9de90758b0987097c631937a2b3530b6
stackline <stackline@users.noreply.github.com>
2020-11-10 11:16:36 +0900

    ABC181 C explanation (refactor data structure and comment)

--- abc/abc181/c_exp.cc
+++ abc/abc181/c_exp.cc
@@ -8,7 +8,7 @@ int main() {
   int n;
   cin >> n;
 
-  vector<pair<ll, ll>> p(n);
+  vector<pair<int, int>> p(n);
   for (int i = 0; i < n; i++) {
     int x, y;
     cin >> x >> y;
@@ -31,36 +31,37 @@ int main() {
         //
         //   A(0, 0), B(x1, y1), C(x2, y2)
         //
-        // 直線 AB と直線 AC の傾きが等しい場合、
+        // 「直線 AB の傾き = 直線 AC の傾き」が成立する場合、
         // A, B, C は同一直線上に存在する。
         //
-        // AB, AC の傾きは以下である。
+        // 直線 AB、直線 AC の傾きはそれぞれ以下である。
         //
-        //   * ABの傾き = y1 / x1
-        //   * ACの傾き = y2 / x2
+        //   * 直線 AB の傾き = y1 / x1
+        //   * 直線 AC の傾き = y2 / x2
         //
-        // これより、以下が成立するとよい。
+        // これらより、以下が成立する場合、A, B, C が
+        // 同一直線上に存在する。
         //
         //   (y1 / x1) == (y2 / x2)
         //
-        // 除算による小数の誤差発生を回避するため、
-        // 乗算した値同士の比較に変える。
+        // 除算による小数の取り扱いを回避するため、
+        // 乗算した値同士の比較に、式を変更する。
         //
         //   y1 * x2 == y2 * x1
         //
-        ll x1, y1, x2, y2, x3, y3;
+        int x1, y1, x2, y2, x3, y3;
         tie(x1, y1) = p.at(i);
         tie(x2, y2) = p.at(j);
         tie(x3, y3) = p.at(k);
 
-        x1 = x1 - x3;
-        y1 = y1 - y3;
-        x2 = x2 - x3;
-        y2 = y2 - y3;
-        // x3 = x3 - x3;
-        // y3 = y3 - y3;
+        int dx1 = x1 - x3;
+        int dy1 = y1 - y3;
+        int dx2 = x2 - x3;
+        int dy2 = y2 - y3;
+        // int dx3 = x3 - x3;
+        // int dy3 = y3 - y3;
 
-        if (y1 * x2 == y2 * x1) {
+        if (dy1 * dx2 == dy2 * dx1) {
           cout << "Yes" << endl;
           return 0;
         }
