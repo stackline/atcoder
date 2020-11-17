9cd0384f44ad6098ab336453041e6628ff50632e
stackline <stackline@users.noreply.github.com>
2020-11-18 01:31:41 +0900

    ABC182 A explanation (do not add unnecessary if statement)

---
+++ abc/abc182/a_exp.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  cin.tie(nullptr); // Do not flush "cout" when processing "cin".
+
+  int a, b;
+  cin >> a >> b;
+
+  cout << ((2 * a + 100) - b) << endl;
+  return 0;
+}
