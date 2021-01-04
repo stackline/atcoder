4a2433ecbd5cf857d7027cbf961279712b3298dd
stackline <stackline@users.noreply.github.com>
2021-01-04 13:06:58 +0900

    ABC184 D memoization

--- abc/abc184/d.cc
+++ abc/abc184/d.cc
@@ -46,6 +46,43 @@ double search(int a, int b, int c, int cnt, double rate) {
   return expected_a + expected_b + expected_c;
 }
 
+double memoize(int a, int b, int c, int cnt, double rate,
+               vector<vector<vector<bool>>> &done,
+               vector<vector<vector<double>>> &memo) {
+  // ベースケース
+  if (a == 100 || b == 100 || c == 100) {
+    return cnt * rate;
+  }
+
+  // 再帰ステップ
+
+  // ---------- メモ化 ----------
+  if (done.at(a).at(b).at(c)) {
+    // printf("[cache hit] a: %d, b: %d, c: %d\n", a, b, c);
+    return memo.at(a).at(b).at(c);
+  }
+  // ---------- メモ化 ----------
+
+  // 金貨を引いた場合
+  double rate_a = rate * a / (a + b + c);
+  double expected_a = memoize(a + 1, b, c, cnt + 1, rate_a, done, memo);
+  // 銀貨を引いた場合
+  double rate_b = rate * b / (a + b + c);
+  double expected_b = memoize(a, b + 1, c, cnt + 1, rate_b, done, memo);
+  // 銅貨を引いた場合
+  double rate_c = rate * c / (a + b + c);
+  double expected_c = memoize(a, b, c + 1, cnt + 1, rate_c, done, memo);
+
+  double expected = expected_a + expected_b + expected_c;
+
+  // ---------- メモ化 ----------
+  done.at(a).at(b).at(c) = true;
+  memo.at(a).at(b).at(c) = expected;
+  // ---------- メモ化 ----------
+
+  return expected;
+}
+
 int main() {
   cin.tie(nullptr); // Do not flush "cout" when processing "cin".
 
@@ -53,7 +90,15 @@ int main() {
   cin >> a >> b >> c;
 
   // 全探索
-  double val = search(a, b, c, 0, 1.0);
+  // double val = search(a, b, c, 0, 1.0);
+
+  // メモ化
+  // すでに計算したかどうか
+  vector<vector<vector<bool>>> done(
+      101, vector<vector<bool>>(101, vector<bool>(101)));
+  vector<vector<vector<double>>> memo(
+      101, vector<vector<double>>(101, vector<double>(101)));
+  double val = memoize(a, b, c, 0, 1.0, done, memo);
   cout << fixed << setprecision(9);
   cout << val << endl;
 
