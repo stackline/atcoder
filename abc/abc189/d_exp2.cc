a8cf2b8148b7359d7e8ecec2ae70c215ffcb6af9
stackline <stackline@users.noreply.github.com>
2021-02-11 15:29:55 +0900

    ABC189 D explanation second (refactor)

--- abc/abc189/d_exp2.cc
+++ abc/abc189/d_exp2.cc
@@ -20,21 +20,32 @@ int main() {
     cin >> s;
 
     // y0 (= x0) と x1 の組み合わせから、y1 の取りうるパターン数を数える
+    // for (bool y_current : {true, false}) {
+    //   bool y_next;
+    //   if (s == "AND") {
+    //     // AND true の場合
+    //     y_next = y_current && true;
+    //     dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
+    //     // AND false の場合
+    //     y_next = y_current && false;
+    //     dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
+    //   } else {
+    //     // OR true の場合
+    //     y_next = y_current || true;
+    //     dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
+    //     // AND false の場合
+    //     y_next = y_current || false;
+    //     dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
+    //   }
+    // }
     for (bool y_current : {true, false}) {
-      bool y_next;
-      if (s == "AND") {
-        // AND true の場合
-        y_next = y_current && true;
-        dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
-        // AND false の場合
-        y_next = y_current && false;
-        dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
-      } else {
-        // OR true の場合
-        y_next = y_current || true;
-        dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
-        // AND false の場合
-        y_next = y_current || false;
+      for (bool x_next : {true, false}) {
+        bool y_next;
+        if (s == "AND") {
+          y_next = y_current && x_next;
+        } else {
+          y_next = y_current || x_next;
+        }
         dp.at(i + 1).at(y_next) += dp.at(i).at(y_current);
       }
     }
